8e95d16aadb1ca7464f9d9fbbb1edf65f62463c9
sinegami301194 <harrymgtu@gmail.com>
2019-04-25 18:48:23 +0300

    Add files via upload

---
+++ 1 week/Pin_image.py
@@ -0,0 +1,11 @@
+N = int(input())
+P1 = "   _~_    "
+P2 = "  (o o)   "
+P3 = " /  V  \  "
+P4 = "/(  _  )\ "
+P5 = "  ^^ ^^   "
+print(P1 * N)
+print(P2 * N)
+print(P3 * N)
+print(P4 * N)
+print(P5 * N)
