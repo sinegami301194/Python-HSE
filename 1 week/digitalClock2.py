1be7465eae93bfdff3554ed37b7cad6d88653cfa
sinegami301194 <harrymgtu@gmail.com>
2019-04-26 12:11:00 +0300

    Add files via upload

---
+++ 1 week/digitalClock2.py
@@ -0,0 +1,11 @@
+N = int(input())
+ss = N % 60
+s1 = ss // 10
+s2 = ss % 10
+mm1 = N // 60
+mm = mm1 % 60
+m1 = mm // 10
+m2 = mm % 10
+hh1 = N // 3600
+hh = hh1 % 24
+print(hh, ':', m1, m2, ':', s1, s2, sep='')
