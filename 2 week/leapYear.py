6878d52489ffb33f5fc2eaab10ef31288089522b
sinegami301194 <harrymgtu@gmail.com>
2019-04-26 18:28:17 +0300

    Add files via upload

---
+++ 2 week/leapYear.py
@@ -0,0 +1,11 @@
+A = int(input())
+if A % 400 == 0:
+    print('YES', end='')
+else:
+    if A % 100 == 0:
+        print('NO', end='')
+    else:
+        if A % 4 == 0:
+            print('YES', end='')
+        else:
+            print('NO', end='')
