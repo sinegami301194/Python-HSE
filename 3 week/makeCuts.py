11bfffef4cf64a0edc5c9078f1afa4d73cfd48af
Sinegami <harrymgtu@gmail.com>
2019-04-30 20:51:53 +0300

    Add files via upload

---
+++ 3 week/makeCuts.py
@@ -0,0 +1,11 @@
+str = input()
+print(str[2])
+print(str[-2])
+print(str[:5])
+print(str[:-2])
+print(str[::2])
+print(str[1::2])
+print(str[::-1])
+newStr = str[::-1]
+print(newStr[::2])
+print(len(str), end='')
