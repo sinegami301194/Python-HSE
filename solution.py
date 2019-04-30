a01ba9ec019b383b624b67e284cbba4aace9f1e7
Sinegami <harrymgtu@gmail.com>
2019-04-30 12:33:47 +0300

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,20 @@
+import math
+a, b, c = float(input()), float(input()), float(input())
+eps = 10**(-6)
+D = b**2 - 4 * a * c
+if D - eps > 0:
+    x1 = (-b - math.sqrt(D)) / (2 * a)
+    x2 = (-b + math.sqrt(D)) / (2 * a)
+    if x1 % 1 == 0:
+        x1 = round(x1)
+    if x2 % 1 == 0:
+        x2 = round(x2)
+    if x1 > x2:
+        print(x2, x1, end='')
+    else:
+        print(x1, x2, end='')
+elif D < eps and D > -eps:
+    x = -b / (2 * a)
+    if x % 1 == 0:
+        x = round(x)
+    print(x, end='')
