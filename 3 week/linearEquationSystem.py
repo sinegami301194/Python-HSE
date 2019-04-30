c207d4e26852c24e9ee9ac4e17394c32dbcb7500
Sinegami <harrymgtu@gmail.com>
2019-04-30 13:46:07 +0300

    Add files via upload

---
+++ 3 week/linearEquationSystem.py
@@ -0,0 +1,13 @@
+a, b, c = float(input()), float(input()), float(input())
+d, e, f = float(input()), float(input()), float(input())
+if b != 0:
+    y = (c * e - a * f) / (b * c - a * d)
+    if c != 0:
+        x = (f - d * y) / c
+        print(x, y, end='')
+else:
+    if a != 0:
+        x = (f * b - d * e) / (b * c - d * a)
+        if d != 0:
+            y = (f - c * x) / d
+            print(x, y, end='')
