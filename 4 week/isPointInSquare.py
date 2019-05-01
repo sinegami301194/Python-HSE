9e67b3277af78308f838577bfbec3bb07e735c19
Sinegami <harrymgtu@gmail.com>
2019-05-01 17:00:52 +0300

    Add files via upload

---
+++ 4 week/isPointInSquare.py
@@ -0,0 +1,10 @@
+def IsPointInSquare(x, y):
+    if x >= -1 and x <= 1 and y >= -1 and y <= 1:
+        return True
+    return False
+x = float(input())
+y = float(input())
+if IsPointInSquare(x, y):
+    print('YES', end='')
+else:
+    print('No', end='')
