8c7d1db2e6d244cf0ad3d9a22b006c120856cfe4
Sinegami <harrymgtu@gmail.com>
2019-05-01 19:40:56 +0300

    Delete firstAndLastEntry.py

--- firstAndLastEntry.py
+++
@@ -1,8 +0,0 @@
-str = input()
-posBegin = str.find('f', 0)
-if str.find('f', posBegin + 1) != -1:
-    newStr = str[::-1]
-    posEnd = len(str) - newStr.find('f', 0) - 1
-    print(posBegin, posEnd, end='')
-if str.find('f', posBegin + 1) == -1 and posBegin != -1:
-    print(posBegin, end='')
