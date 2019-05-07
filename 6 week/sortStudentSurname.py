52b32e21cf0012358b09fb21ad5ac3ba583cff1e
Sinegami <harrymgtu@gmail.com>
2019-05-07 22:31:28 +0300

    Add files via upload
    Отсортировать список участников по алфавиту
    
    Известно, что фамилии всех участников — различны. Сохраните в массивах список всех участников и выведите его, отсортировав по фамилии в лексикографическом порядке. При выводе указываете фамилию, имя участника и его балл.
    
    Используйте для ввода и вывода файлы input.txt и output.txt с указанием кодировки utf8. Например, для чтения откройте файл с помощью open('input.txt', 'r', encoding='utf8').
    
    Входные данные
    
    Строки вида "Фамилия Имя НомерШколы Балл".
    
    Выходные данные
    
    Строки вида "Фамилия Имя Балл", отсортированные по фамилии.

---
+++ 6 week/sortStudentSurname.py
@@ -0,0 +1,35 @@
+readFromFile = open('input.txt', 'r', encoding='utf8')
+writeInFile = open('output.txt', 'w', encoding='utf8')
+
+
+class Student:
+    Surname = ''
+    Name = ''
+    school = 0
+    score = 0
+
+
+def sortStudent(A):
+    for j in range(0, len(A) - 1):
+        for i in range(0, len(A) - 1):
+            if A[i].Surname > A[i+1].Surname:
+                A[i], A[i + 1] = A[i + 1], A[i]
+        A.sort
+    return A
+peopleList = []
+A = []
+lines = readFromFile.readlines()
+for line in lines:
+    peopleList.extend(list(line.split()))
+for i in range(len(peopleList) // 4):
+    stud = Student()
+    stud.Surname = str(peopleList[4 * i])
+    stud.Name = str(peopleList[4 * i + 1])
+    stud.school = int(peopleList[4 * i + 2])
+    stud.score = int(peopleList[4 * i + 3])
+    A.append(stud)
+B = sortStudent(A)
+for i in range(0, len(B)):
+    print(B[i].Surname, B[i].Name, B[i].score, file=writeInFile)
+readFromFile.close()
+writeInFile.close()
