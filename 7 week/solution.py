b7fdd0450e4e3090f11ab3d4d8015a4705910a56
Sinegami <harrymgtu@gmail.com>
2019-05-10 02:52:25 +0300

    Add files via upload

---
+++ 7 week/solution.py
@@ -0,0 +1,14 @@
+union = set()
+all = set()
+for i in range(int(input())):
+    m = int(input())
+    a = {input() for j in range(m)}
+    all.update(a)
+    if i == 1:
+        union.update(a)
+    else:
+        union &= a
+print(len(union))
+print('\n'.join(sorted(union)))
+print(len(all))
+print('\n'.join(sorted(all)))
