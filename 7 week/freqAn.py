c64d50750497914cdce52653877cc47a78d54625
Sinegami <harrymgtu@gmail.com>
2019-05-11 00:20:04 +0300

    Update freqAn.py

--- 7 week/freqAn.py
+++ 7 week/freqAn.py
@@ -1,17 +1,16 @@
 import sys
- 
+
 myList = (str(sys.stdin.read()).split())
 myLets = {}
 for word in myList:
     myLets[word] = myLets.get(word, 0) + 1
 myLets = [(fr, lt) for lt, fr in myLets.items()]
- 
- 
+
+
 def makeSort(lws):
     return (-lws[0], lws[1])
- 
- 
+
+
 myLets.sort(key=makeSort)
 for i in range(len(myLets)):
     print(myLets[i][1])
-    
