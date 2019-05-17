4fd119abdb02b7e8b9b42f5fd1ff10db01bcf206
Sinegami <harrymgtu@gmail.com>
2019-05-17 23:30:45 +0300

    Update cost.py

--- 3 week/cost.py
+++ 3 week/cost.py
@@ -1,4 +1,4 @@
 a = float(input()) # Creating the float number
-rub = int(a // 1)
-kop = round((a % 1) * 100)
-print(rub, kop, end='')
+rub = int(a // 1) # Solving the rub
+kop = round((a % 1) * 100) # Solving the kop
+print(rub, kop, end='') # Print result
