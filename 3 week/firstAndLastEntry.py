501c3bd73ca967adf3bde93db17988f9d2f7fdb0
Sinegami <harrymgtu@gmail.com>
2019-05-17 23:36:55 +0300

    Update firstAndLastEntry.py

--- 3 week/firstAndLastEntry.py
+++ 3 week/firstAndLastEntry.py
@@ -1,5 +1,5 @@
-str = input()
-posBegin = str.find('f', 0)
+str = input() # Input a string
+posBegin = str.find('f', 0) # Find first position of symbol in inputed string
 if str.find('f', posBegin + 1) != -1:
     newStr = str[::-1]
     posEnd = len(str) - newStr.find('f', 0) - 1
