3a228fb8820c7584404e575c4bfbd915c034b6bf
Sinegami <harrymgtu@gmail.com>
2019-06-08 18:45:47 +0300

    Update king.py

--- 2 week/king.py
+++ 2 week/king.py
@@ -1,7 +1,7 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
+x1 = int(input()) # Input the number
+y1 = int(input()) # Input the number
+x2 = int(input()) # Input the number
+y2 = int(input()) # Input the number
 if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1:
     print('YES', end='')
 else:
